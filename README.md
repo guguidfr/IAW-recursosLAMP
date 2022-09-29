0737674eb80c56c4c9100c9921e121c2de2d1d9f
guguidfr <guguidfr@protonmail.com>
2022-09-29 21:53:11 +0200

    Recursos para empezar con el LAMP

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Recursos
+El script actualiza los repositorios e instala:  
+- Actualiza los repositorios y los paquetes.
+- Instala *neofetch*, *curl* y *zip*
+- Para el LAMP, deja preinstalados:
+    - Apache
+    - PHP 7.4 y todos los paquetes necesarios
+    - MariaDB, cliente y servidor
+> También crea el archivo ***/var/www/html/info.php***  
+> y añade código de PHP con información.
+### Acerca del fichero: index.html
+Es un archivo *.html* simple con enlaces de redirección a los gestores de contenido.
+### Acerca del fichero info.txt  
+Contiene credenciales genéricas para un usuario en cada uno de los CMS: *WordPress*, *Moodle* y *NextCloud*.
--- setup.sh
+++ setup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 sudo apt update && sudo apt -y upgrade
-sudo apt install -y neofetch && sudo apt install -y git
+sudo apt install -y neofetch #&& sudo apt install -y git
 #echo "vagrant:vagrant" | sudo chpasswd
 #git clone https://github.com/guguidfr/get-ansible-debian.git
 #git clone https://github.com/guguidfr/Proyecto.git
 #sh ./get-ansible-debian/get-ansible.sh
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo systemctl restart sshd;
+#sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+#sudo systemctl restart sshd;
 sudo apt install -y curl
-sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common gnupg2
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
-curl -fsSL  https://packages.sury.org/php/apt.gpg| sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
-sudo apt update && sudo apt -y upgrade
+#sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common gnupg2
+#echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+#curl -fsSL  https://packages.sury.org/php/apt.gpg| sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+#sudo apt update && sudo apt -y upgrade
 sudo apt install -y zip
 sudo apt install -y apache2 apache2-utils
-sudo apt-get install -y php libapache2-mod-php php-mysql php-common php-cli php-common php-json php8.1-opcache php-readline
-sudo a2enmod php8.1
+sudo apt install -y php7.4
+sudo apt install -y php7.4-{curl,gd,intl,mbstring,soap,xml,xmlrpc,zip}
+#sudo a2enmod php8.1
+sudo touch /var/www/html/info.php
 sudo chown www-data:www-data /var/www/html/ -R
-echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
+sudo echo "<?php phpinfo(); ?>" >> /var/www/html/info.php
 sudo apt install -y mariadb-server mariadb-client
 systemctl restart apache2
 sudo apt update && sudo apt -y upgrade
